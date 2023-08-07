d3b07ad00495417961b4168c07f3e02786c2c311
hohnchan <ciwei17@gmail.com>
2023-08-08 00:03:13 +0800

    update

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -20,4 +20,16 @@ struct Landmark:Hashable,Codable{
     var image:Image{
         Image(imageName)
     }
+    
+    private var coordinates: Coordinates
+
+        struct Coordinates: Hashable, Codable {
+            var latitude: Double
+            var longitude: Double
+        }
+    
+    struct Coordinates: Hashable, Codable {
+            var latitude: Double
+            var longitude: Double
+        }
 }
