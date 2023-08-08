09804a53bef8a08b964dd275935b277117c41a75
hohnchan <ciwei17@gmail.com>
2023-08-08 23:57:39 +0800

    update

--- Landmarks/LandmarkRow.swift
+++ Landmarks/LandmarkRow.swift
@@ -11,12 +11,24 @@ struct LandmarkRow: View {
     var landmark: Landmark
     
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50,height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
     }
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkRow(landmark: landmarks[0])
+        
+        VStack{
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+        
     }
 }
