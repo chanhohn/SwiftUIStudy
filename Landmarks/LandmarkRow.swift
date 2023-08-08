197ad705d76ec9294c4ca52ac4b8a8b73f3da7d8
hohnchan <ciwei17@gmail.com>
2023-08-08 23:42:03 +0800

    update

--- Landmarks/LandmarkRow.swift
+++ Landmarks/LandmarkRow.swift
@@ -8,13 +8,15 @@
 import SwiftUI
 
 struct LandmarkRow: View {
+    var landmark: Landmark
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
     }
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkRow()
+        LandmarkRow(landmark: landmarks[0])
     }
 }
